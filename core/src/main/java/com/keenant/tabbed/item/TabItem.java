b8bee907b11b1396bc31da371b79ec2d0e5f8ffb
A248 <anandebeh@gmail.com>
2020-03-31 11:04:47 -0400

    Add more documentation to TabItem

--- core/src/main/java/com/keenant/tabbed/item/TabItem.java
+++ core/src/main/java/com/keenant/tabbed/item/TabItem.java
@@ -6,6 +6,7 @@ import com.keenant.tabbed.skin.Skin;
  * Represents a custom tab item.
  */
 public interface TabItem {
+	
     /**
      * The text of the tab item (any length, recommended less than ~18). No calculations should be made.
      * 
@@ -28,21 +29,26 @@ public interface TabItem {
     Skin getSkin();
 
     /**
-     * Makes any appropriate changes to the text.
+     * Attempts to update the text of this tab item. <br>
+     * The updateXXX (updateText(), updatePing(), updateSkin()) methods are called
+     * when enclosing <code>TabList</code>s desires to check for updates.
+     * <code>true</code> indicates a change has been made and the tab list should be updated.
      * 
      * @return true if a change has been made, false otherwise
      */
     boolean updateText();
 
     /**
-     * Makes any appropriate changes to the ping.
+     * Attempts to update the ping of this tab item. <br>
+     * See {@link #updateText()}
      * 
      * @return true if a change has been made, false otherwise
      */
     boolean updatePing();
 
     /**
-     * Makes any appropriate changes to the skin.
+     * Attempts to update the skin of this tab item. <br>
+     * See {@link #updateSkin()}
      * 
      * @return true if a change has been made, false otherwise
      */
